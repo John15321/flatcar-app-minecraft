daef26369e9ef3a83295e5dcbb512c7267e9bd81
Jan Bronicki <janbronicki@microsoft.com>
2024-12-19 22:14:09 +0100

    Add compiling with butane to config script and specify RAM size

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./flatcar_production_qemu.sh -M  6144 -i config.json -- -display curses
+docker run --rm -i quay.io/coreos/butane:latest < config.yaml > config.json ; ./flatcar_production_qemu.sh -M 8192 -i ./config.json -- -display curses
